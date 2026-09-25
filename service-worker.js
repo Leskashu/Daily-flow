bbe261b4b2e7a24eea765de348290eae03a049cb
Leskashu <105780019+Leskashu@users.noreply.github.com>
2026-09-25 11:24:24 +0400

    chore: refresh PWA shell v13

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'daily-flow-v12';
-const APP_SHELL = ['./', './index.html', './styles.css?v=mobile-r12', './polish.css?v=mobile-r12', './app.js?v=mobile-r12', './manifest.json', './icon.svg'];
+const CACHE_NAME = 'daily-flow-v13';
+const APP_SHELL = ['./', './index.html', './styles.css?v=mobile-r13', './polish.css?v=mobile-r13', './app.js?v=mobile-r13', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
